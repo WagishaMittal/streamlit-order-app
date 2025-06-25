b8fdf978b021dee098d19669260819c6805a3812
WagishaMittal <wagishamtl2002@gmail.com>
2025-06-25 14:51:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from google.oauth2.service_account import Credentials
 # Load from Google Sheets
 def load_sheet():
     creds_dict = st.secrets["gcp_service_account"]
-    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    st.write("Service account loaded:", creds["client_email"])
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     credentials = Credentials.from_service_account_info(creds_dict)
     gc = gspread.authorize(credentials)
